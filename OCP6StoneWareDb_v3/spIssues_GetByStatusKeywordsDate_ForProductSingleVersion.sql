72536f97fdcd186497c4486bc0b2dbf4837431b9
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-09-25 19:31:42 -0400

    Create spIssues_GetByStatusKeywordsDate_ForProductSingleVersion.sql

---
+++ OCP6StoneWareDb_v3/spIssues_GetByStatusKeywordsDate_ForProductSingleVersion.sql
@@ -0,0 +1,31 @@
+--=========================================================================
+-- Get all issues for a product by status, date range, version and keywords
+--==========================================================================
+USE [OCP6StoneWareDB_V3]
+GO
+
+CREATE OR ALTER PROCEDURE spIssues_GetByStatusKeywordsDate_ForProductSingleVersion(
+		@product_name nvarchar(50),
+		@version numeric(2,1), 
+		@status nvarchar(50) = 'Outstanding', 
+		@keywords nvarchar(MAX),
+		@start_date date = '2010-01-01',
+		@end_date date = null)
+AS
+BEGIN
+	IF @end_date IS NULL
+	BEGIN
+		SET @end_date = GetDate()
+	END
+
+	SELECT * FROM VWIssueGetAll AS vw
+		WHERE status = @status
+		AND product = @product_name
+		AND version = @version
+		AND created_at BETWEEN @start_date AND @end_date
+	AND 
+	(
+		EXISTS (SELECT Keyword FROM dbo.fnSplitList(@keywords) WHERE vw.resolution_description LIKE '%'+Keyword+'%')
+		OR EXISTS (SELECT Keyword FROM dbo.fnSplitList(@keywords) WHERE vw.problem_description LIKE '%'+Keyword+'%')
+	)
+END
