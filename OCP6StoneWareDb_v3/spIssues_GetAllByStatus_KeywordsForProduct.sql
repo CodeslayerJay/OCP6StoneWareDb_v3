0af8246eb2c3e7d990937e5c5410df04f23edaa0
Jeremy Willhelm <46633603+CodeslayerJay@users.noreply.github.com>
2019-09-25 19:05:25 -0400

    added store procedure

---
+++ OCP6StoneWareDb_v3/spIssues_GetAllByStatus_KeywordsForProduct.sql
@@ -0,0 +1,19 @@
+--====================================================
+-- Get all issues for a product by status and keywords
+--====================================================
+USE [OCP6StoneWareDB_V3]
+GO
+
+CREATE OR ALTER PROCEDURE spIssues_GetAllByStatus_KeywordsForProduct(
+		@product_name nvarchar(50), @status nvarchar(50) = 'Outstanding', @keywords nvarchar(MAX))
+AS
+BEGIN
+	SELECT * FROM VWIssueGetAll AS vw
+		WHERE status = @status
+		AND product = @product_name
+	AND 
+	(
+		EXISTS (SELECT Keyword FROM dbo.fnSplitList(@keywords) WHERE vw.resolution_description LIKE '%'+Keyword+'%')
+		OR EXISTS (SELECT Keyword FROM dbo.fnSplitList(@keywords) WHERE vw.problem_description LIKE '%'+Keyword+'%')
+	)
+END
---
+++ OCP6StoneWareDb_v3/spIssues_GetByStatusKeywordsVersion_ForProduct.sql
@@ -0,0 +1,20 @@
+--=============================================================
+-- Get all issues for a product by status, version and keywords
+--=============================================================
+USE [OCP6StoneWareDB_V3]
+GO
+
+CREATE OR ALTER PROCEDURE spIssues_GetByStatusKeywordsVersion_ForProduct(
+		@product_name nvarchar(50), @version numeric(2,1), @status nvarchar(50) = 'Outstanding', @keywords nvarchar(MAX))
+AS
+BEGIN
+	SELECT * FROM VWIssueGetAll AS vw
+		WHERE status = @status
+		AND product = @product_name
+		AND version = @version
+	AND 
+	(
+		EXISTS (SELECT Keyword FROM dbo.fnSplitList(@keywords) WHERE vw.resolution_description LIKE '%'+Keyword+'%')
+		OR EXISTS (SELECT Keyword FROM dbo.fnSplitList(@keywords) WHERE vw.problem_description LIKE '%'+Keyword+'%')
+	)
+END
